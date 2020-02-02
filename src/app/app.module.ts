8641cdb7efe599dc486883ffd90568bcc21ac075
Marlys <sylram7@gmail.com>
2020-02-02 16:37:28 -0500

    routing-basis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,23 +8,28 @@ import { MatGridListModule} from '@angular/material/grid-list'
 import { MatCardModule} from '@angular/material/card'
 import { MatButtonModule} from '@angular/material/button'
 
-
 import 'hammerjs';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishDetailsComponent } from './dish-details/dish-details.component';
-
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
+import { DishService } from './services/dish.service';
+import { AppRoutingModule} from './app-routing/app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     DishDetailsComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -34,7 +39,8 @@ import { FooterComponent } from './footer/footer.component';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [ 
     DishService
